0f31f8a8686a5710fac9c9cd38656e7144f5b8ff
unknown <Uppal@2705>
2024-09-23 17:05:45 +0530

    changre

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -1,9 +1,31 @@
-# release.ps1
+# finalize.ps1
 
-# Create a release branch
+# Ensure the script is run from the project root
+Set-Location -Path (Split-Path -Parent $MyInvocation.MyCommand.Path)
+
+# Read the version number from VERSION.txt
 $version = Get-Content VERSION.txt
-git checkout -b "release/$version"
-git push origin "release/$version"
 
-# Run tests
-pytest
+# Merge the release branch into main
+git checkout main
+git pull origin main
+git merge --no-ff "release/v$version"
+
+# Tag the release
+git tag -a "v$version" -m "Release version $version"
+
+# Push the changes and tags to the remote repository
+git push origin main
+git push origin "v$version"
+
+# Merge the release branch back into develop
+git checkout develop
+git pull origin develop
+git merge --no-ff "release/v$version"
+
+# Push the changes to the remote repository
+git push origin develop
+
+# Delete the release branch locally and remotely
+git branch -d "release/v$version"
+git push origin --delete "release/v$version"
